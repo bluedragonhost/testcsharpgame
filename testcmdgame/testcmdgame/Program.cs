39d7f203631602d7fdd83ac604dbee7530118d3a
Jack Sullivan <jack.sullivan10@education.nsw.gov.au>
2014-10-03 15:46:04 +1000

    Added a notification that the game is only in beta

--- testcmdgame/testcmdgame/Program.cs
+++ testcmdgame/testcmdgame/Program.cs
@@ -19,6 +19,18 @@ namespace testcmdgame
 			#region Customise the Player
 			//coming soon!
 			#endregion
+			#region ExplanationOfTheBeta
+			Console.WriteLine ("This app is only in beta and may have some weirds bugs in it,");
+			Console.WriteLine ("To help the developers out please report all bugs to the issues page");
+			Console.WriteLine ("On Github, Thankyou!"); 
+			#endregion
+			#region GameStart
+			Console.WriteLine ("Link Start");
+			Console.WriteLine ("Player Username: BETATESTPLAYER");
+			Console.WriteLine ("Player Password: abetaisfinetoo");
+			Console.WriteLine ("Credentials Correct!")
+			#endregion
+
 
 
 
