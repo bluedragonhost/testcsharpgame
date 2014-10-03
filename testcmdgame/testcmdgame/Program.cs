0e7d7f3c4107385555cc548da4965a10b86dc6af
Jack Sullivan <jack.sullivan10@education.nsw.gov.au>
2014-10-03 15:37:10 +1000

    Added the apps intro and autosave feature

--- testcmdgame/testcmdgame/Program.cs
+++ testcmdgame/testcmdgame/Program.cs
@@ -6,7 +6,22 @@ namespace testcmdgame
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+
+			//Introduction to the app and waits for a save to commence
+			#region Intro to the app
+			Console.WriteLine ("Hello and Welcome to the TestApp Game");
+			#endregion
+			#region AutoSaveFeature
+			Console.WriteLine ("This app has an autosave feature and will pause and save before continuing OK?");
+			Console.ReadKey ();
+			Console.WriteLine ("The app has successfully saved");
+			#endregion
+			#region Customise the Player
+			//coming soon!
+			#endregion
+
+
+
 		}
 	}
 }
