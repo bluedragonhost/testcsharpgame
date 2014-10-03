4acaab21103da9b7ea6c3ab8797f122458d8ae43
Jack Sullivan <jack.sullivan10@education.nsw.gov.au>
2014-10-03 16:13:19 +1000

    Added an After Start Intro to the game

--- testcmdgame/testcmdgame/Program.cs
+++ testcmdgame/testcmdgame/Program.cs
@@ -24,16 +24,32 @@ namespace testcmdgame
 			//coming soon!
 			#endregion
 			#region ExplanationOfTheBeta
+			//ask people to please report all bugs to the dev team
 			Console.WriteLine ("This app is only in beta and may have some weirds bugs in it,");
-			Thread.Sleep (50); 
+			Thread.Sleep (1000); 
 			Console.WriteLine ("To help the developers out please report all bugs to the issues page");
+			Thread.Sleep (1000); 
 			Console.WriteLine ("On Github, Thankyou!"); 
+			Thread.Sleep (1000);
 			#endregion
 			#region GameStart
+			//sao go!
 			Console.WriteLine ("Link Start");
+			Thread.Sleep (500);
+			//enter the player credentials, until the beta is finished the players will sign in using a beta key
 			Console.WriteLine ("Player Username: BETATESTPLAYER");
+			//wait a bit to make the typing look realistic
+			Thread.Sleep (500);
+			//oh, wow what a interesting password!
 			Console.WriteLine ("Player Password: abetaisfinetoo");
+			//sleep the thread again
+			Thread.Sleep (500);
+			//say that the credentials are correct
 			Console.WriteLine ("Credentials Correct!");
+			Thread.Sleep (200)
+			#endregion
+			#region AfterGameStartIntro
+			//coming soon
 			#endregion
 
 
